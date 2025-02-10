eb3c60ed32248d496d9b2f1ab1225a50f86bba57
Ekramul <mdekramulhassan168@gmail.com>
2025-02-10 19:46:16 +0600

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>
+        <Button>it is ok</Button>
+      </h1>
+    </>
+  );
+}
+
+export default App;
