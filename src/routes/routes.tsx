862a24c7385338c84070f22395a34a0ad2968a81
Ekramul <mdekramulhassan168@gmail.com>
2025-02-11 21:35:49 +0600

    some code modyfied

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,24 +1,25 @@
-import App from "@/App";
 import { routeGenerator } from "@/utils/RouteHandler";
 import { createBrowserRouter } from "react-router-dom";
 import { adminPaths } from "./admin.routes";
 import { customerPaths } from "./customer.routes";
 import Login from "@/page/Login";
 import Signup from "@/page/Register";
+import AdminDashboard from "@/page/admin/AdminDashboard";
+import CustomerDashboard from "@/page/customers/CustomerDashbord";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Login />,
   },
   {
     path: "/admin",
-    element: <App />,
+    element: <AdminDashboard />,
     children: routeGenerator(adminPaths),
   },
   {
     path: "/customer",
-    element: <App />,
+    element: <CustomerDashboard />,
     children: routeGenerator(customerPaths),
   },
   {
